6c0765ab938b94880bf628add5bc653b52f5a225
Titus Wormer <tituswormer@gmail.com>
2016-11-05 13:24:40 +0100

    Remove file-overview inline docs

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-/**
- * @author Titus Wormer
- * @copyright 2016 Titus Wormer
- * @license MIT
- * @module css-declarations
- * @fileoverview Parse and stringify CSS declarations.
- */
-
 'use strict';
 
 /* Dependencies. */
--- test.js
+++ test.js
@@ -1,11 +1,3 @@
-/**
- * @author Titus Wormer
- * @copyright 2016 Titus Wormer
- * @license MIT
- * @module css-declarations
- * @fileoverview Test suite for `css-declarations`.
- */
-
 'use strict';
 
 /* Dependencies. */
